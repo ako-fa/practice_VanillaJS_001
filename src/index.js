d162e5b1a33aa8f6b5487dee2de4c5587999e930
shotaro_ako <shotaro.ako@frontarc.co.jp>
2022-05-24 07:00:06 +0900

    [delete] コメントの削除

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import "./styles.css";
 
 /**
- * const、letなどの変数宣言
  */
-// var val1 = "var変数";
-// console.log(val1);
 
-// // var変数は上書き可能
-// val1 = "var変数を上書き";
-// console.log(val1);
 
-// // var変数は再宣言可能
-// var val1 = "var変数を再宣言"
-// console.log(val1);
 
-// // let変数
-// let val2 = "let変数";
-// console.log(val2);
 
-// // let変数は再代入可能
-// val2 = "let変数に再代入";
-// console.log(val2);
 
-// // let変数は再宣言不可能
-// let val2 = "let変数を再宣言";
 
